4a792ea107cde7d26e5c23dcf12a8630bef4e556
Ssemugabi Martin <smgmartinez92@gmail.com>
2022-05-08 13:51:59 +0300

    lesson-3

--- lesson-3/js/app.js
+++ lesson-3/js/app.js
@@ -3,5 +3,5 @@ var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-document.getElementById('current-date').innerHTML = ` Last Updated: ${date} ${time}`
+document.querySelector('.current-time').innerHTML = ` Last Updated: ${date} ${time}`
 
