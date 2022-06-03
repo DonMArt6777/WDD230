f3fb0a69502cef1868e445807e5abc6c41332be3
Ssemugabi Martin <76908155+dombreaker36@users.noreply.github.com>
2022-06-03 20:05:04 +0300

    My-updates

--- Chamber/js/windchill.js
+++ Chamber/js/windchill.js
@@ -1,14 +1,19 @@
 const temp = document.querySelector('.current-temp');
 const windspeed = document.querySelector('.wind-speed');
+const wind_chill = document.querySelector('.wind-chill')
 
 
 const calc_wind_chill= () => {
 
   if (temp <=50 || windspeed > 3.0 ) {
-    let f = (35.74+0.6215)*temp-(35.75*8^0.16+0.4275)*temp*8^0.16
+    let wcv = (35.74+0.6215)*temp-(35.75*8^0.16+0.4275)*temp*8^0.16
+  return wcv
+
   }
   
   else {
-    let f = N/A;
+    let wcv = N/A;
+  return wcv
+
   }
 }
