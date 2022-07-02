4501b2de11396e1dd854257d751769b71d9b02ef
Ssemugabi Martin <76908155+dombreaker36@users.noreply.github.com>
2022-07-02 19:01:23 +0300

    Final commit

--- Chamber/js/app.js
+++ Chamber/js/app.js
@@ -88,7 +88,8 @@ todayDisplay.textContent = Date.now()
 
 const requestjson = 'https://dombreaker36.github.io/WDD230/Chamber/js/data.json'
 
-const card = document.querySelector('.cards')
+const myCards = document.querySelector('.cards')
+const spotlight1 = document.querySelector('.grid-3')
 
 
 fetch(requestjson)
@@ -97,13 +98,12 @@ fetch(requestjson)
 }
 )
 .then ((jsonObject)=>{
-  const business = jsonObject["business"]
+  const business = jsonObject["business"];
 
   business.forEach(displayBusiness)
-}
+  
 
-)
-
+})
 const displayBusiness = (business)=>{
   // Create elements to add to the document
   let cards = document.createElement('section');
@@ -136,6 +136,77 @@ const displayBusiness = (business)=>{
  
 
   // Append the existing HTML div with the cards class with the section(card)
-  card.appendChild(cards);
+  myCards.appendChild(cards);
 }
+
+const spotlightarr = [
+  {
+    "companyName": "TOSHA UGANDA LIMITED",
+    "Address": "371233 Makindye Road ",
+    "phoneNumber": "+256-767-7889",
+    "imgaeurl": "https://dombreaker36.github.io/WDD230/Chamber/images/imagen.jpg",
+    "membership": "Gold"
+},
+{
+    "companyName": "RWENZORI UGANDA LIMITED",
+    "Address": "37847 Jinja road ",
+    "phoneNumber": "+256-767-7889",
+    "imgaeurl": "https://dombreaker36.github.io/WDD230/Chamber/images/imagef.jpg",
+    "membership": "None Profitable"
+},
+{
+    "companyName": "ICE UGANDA LIMITED",
+    "Address": "37849 Kampala Social Security House 4th Floor ",
+    "phoneNumber": "+256-767-7889",
+    "imgaeurl": "https://dombreaker36.github.io/WDD230/Chamber/images/imagej.jpg",
+    "membership": "Bronze"
+}
+]
+
+
+
+  const displaySpotlight = (business)=>{
+  // Create elements to add to the document
+  let spotlight = document.createElement('div');
+
+    spotlight.classList.add('spotlight-1')
+
+  let h2 = document.createElement('h2')
+
+  let Adress = document.createElement('p');
+    Adress.classList.add('address')
+  let membership = document.createElement('p')
+  membership.classList.add('member')
+
+  let phoneNumber = document.createElement('p');
+  Adress.textContent= `Address: ${business.Address}`
+  membership.textContent = `Membership: ${business.membership}`
+
+  phoneNumber.textContent = `Phone Number: ${business.phoneNumber}`
+
+  let portrait = document.createElement('img')
+
+  h2.textContent = `${business.companyName}`;
+
+  portrait.setAttribute('src', business.imgaeurl);
+  portrait.setAttribute('alt', `portrait of ${business.companyName}`)
+  portrait.setAttribute('loading', 'lazy');
+
+  // Add/append the section(card) with the h2 element
+  spotlight.appendChild(h2);
+  spotlight.appendChild(portrait)
+  spotlight.appendChild(Adress)
+  spotlight.appendChild(phoneNumber)
+  
+  spotlight.appendChild(membership)
+ 
+
+  // Append the existing HTML div with the cards class with the section(card)
+  spotlight1.appendChild(spotlight);
+}
+
+console.log(spotlightarr)
+
+
+spotlightarr.forEach(displaySpotlight)
 
